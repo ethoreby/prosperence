38014cd23ddde869c3311906ff4b7c3e7a92dacd
Elliott Thoreby <ethoreby@gmail.com>
2015-03-08 13:31:11 -0700

    add comment

--- client/app/university/favorites/favorites.controller.js
+++ client/app/university/favorites/favorites.controller.js
@@ -2,6 +2,8 @@
 
 angular.module('prosperenceApp')
 .controller('FavoritesCtrl', function($scope) {
+
+  //TODO suspect filtering is affected by how videos populated. May not work until resolved.
   $scope.isFavorite = function(youtubeId) {
     return function(course) {
       return $scope.currentUser.university.favorites[youtubeId];
